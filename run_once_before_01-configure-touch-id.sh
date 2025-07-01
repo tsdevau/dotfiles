4f77c8d6f63d124b457b802ba5a77f2c3e954abe
Tim Stone <tim@tsdev.au>
2025-07-01 13:42:51 +1000

    chezmoi: finalise initial configuration

--- run_once_before_01-configure-touch-id.sh
+++ run_once_before_01-configure-touch-id.sh
@@ -2,6 +2,12 @@
 
 # Add pam_tid.so and pam_watchid.so to the PAM sudo_local configuration
 echo "Configuring Touch ID for sudo..."
+
+# Make sure that sudo_local actually exists
+if [[ ! -f  "/etc/pam.d/sudo_local" ]]; then
+  sudo touch "/etc/pam.d/sudo_local"
+fi
+
 sudo sed -i '' 's/^#\?\(auth\s\+sufficient\s\pam_tid.so\)/auth sufficient pam_tid.so/' /etc/pam.d/sudo_local
 sudo sed -i '' 's/^#\?\(auth\s\+sufficient\s\pam_watchid.so\)/auth sufficient pam_watchid.so/' /etc/pam.d/sudo_local
 echo "Touch ID configuration for sudo completed."
