ae971a7c6d3b84344248248267cae489a2394f63
Tim Stone <tim@tsdev.au>
2025-07-03 17:37:26 +1000

    Update .config/yazi/opener-editor-new-tab.sh Update .config/yazi/opener-lazygit-new-tab.sh

--- private_dot_config/private_yazi/opener-editor-new-tab.sh
+++ private_dot_config/private_yazi/opener-editor-new-tab.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Get the filepath from the argument
 filepath="$1"
 
 if [ -d "$filepath" ]; then
- cwd_path=$(realpath "$filepath")
- else
- cwd_path=$(realpath "$(dirname "$filepath")")
- fi
+  cwd_path=$(realpath "$filepath")
+else
+  cwd_path=$(realpath "$(dirname "$filepath")")
+fi
 
 # Get the editor, default to vi if EDITOR is not set
 editor="${EDITOR:-vi}"
 
 # Run the editor with the file
 wezterm cli spawn --cwd "$cwd_path" -- "$editor" "$filepath"
-
--- private_dot_config/private_yazi/opener-lazygit-new-tab.sh
+++ private_dot_config/private_yazi/opener-lazygit-new-tab.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Get the filepath from the argument
 filepath="$1"
 
 if [ -d "$filepath" ]; then
- cwd_path=$(realpath "$filepath")
- else
-   exit 0
- fi
+  cwd_path=$(realpath "$filepath")
+else
+  exit 0
+fi
 
 # Run LazyGit with the directory path
 wezterm cli spawn --cwd "$cwd_path" -- lazygit --path "$filepath"
